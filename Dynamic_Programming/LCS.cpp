b078092e746ed9f0d9fa324e27943027cd22d7ce
Rahul Asati <rahulasati0102@gmail.com>
2024-09-13 22:18:52 +0530

    day5

--- Dynamic_Programming/LCS.cpp
+++ Dynamic_Programming/LCS.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 using namespace std; 
 
-int f(int index1 ,int index2 ,string s,string t, vector<vector<int> > &dp){
+int f(int index1 ,int index2 ,string &s,string &t, vector<vector<int> > &dp){
 	if(index1 < 0 || index2 <0){
 		return 0;
 	}
@@ -26,3 +26,13 @@ int main(){
     cout<<ans;
 	
 }
+
+
+
+// Time Complexity: O(n^2)
+// Space Complexity: O(n^2)
+// * Define base case that is when index1 < 0 or index2 < 0 return 0 because it means the string is empty.
+//  there the is the condition if both the characters are same then return 1 + f(index1-1,index2-1,s,t,dp) because we have found a common character.
+//  else return max(f(index1-1,index2,s,t,dp) , f(index1,index2-1,s,t,dp)) because we have to find the maximum length of common subsequence.
+// * Use memoization to store the result of subproblems to avoid recomputation of same subproblems.
+// * Return the length of longest common subsequence.
